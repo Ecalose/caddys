38acdc213eef84eb1a2c2b2cb0c41a46f8dc5b1f
mixool <mixool0204@gmail.com>
2020-12-06 18:37:14 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,6 +6,6 @@
 go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 
 # xcaddy build
-xcaddy build \
+./xcaddy build \
 	--with github.com/mholt/caddy-l4 \
 	--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
