99dd547ba608d021d09cab2e5fe8e153898b127b
mixool <mixool0204@gmail.com>
2020-12-06 18:25:05 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/go/bin::/usr/local/go/bin; export PATH
 ######
+# only for https://github.com/mixool/caddys/edit/master/generate.sh
+#####
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
@@ -15,6 +17,6 @@ wget -qO $TMPFILE $URL && tar -zxvf $TMPFILE xcaddy && chmod +x xcaddy
 
 # xcaddy build
 ./xcaddy build \
-    --with github.com/mholt/caddy-l4 \
+	--with github.com/mholt/caddy-l4 \
 	--with github.com/caddy-dns/cloudflare \
 	--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
