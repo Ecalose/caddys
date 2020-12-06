78d80010147e58f5d3d05037d183954b7ddb1831
mixool <mixool0204@gmail.com>
2020-12-06 18:38:30 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/go/bin::/usr/local/go/bin; export PATH
 ######
 # only for https://github.com/mixool/caddys/edit/master/generate.sh
 ######
@@ -6,6 +7,6 @@
 go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 
 # xcaddy build
-./xcaddy build \
+xcaddy build \
 	--with github.com/mholt/caddy-l4 \
 	--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
