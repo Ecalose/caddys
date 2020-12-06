42f27b50b7be00fce50f029440895bff5c590c29
mixool <mixool0204@gmail.com>
2020-12-06 18:36:04 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/go/bin::/usr/local/go/bin; export PATH
 ######
 # only for https://github.com/mixool/caddys/edit/master/generate.sh
-#####
+######
 
-# tempfile & rm it when exit
-trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
-
-# go install
-#wget -qO $TMPFILE https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
-#tar -zxvf $TMPFILE
-
-# xcaddy install
-URL="$(wget -qO- https://api.github.com/repos/caddyserver/xcaddy/releases/latest | grep -E "browser_download_url.*linux_amd64.tar.gz" | cut -f4 -d\")"
-wget -qO $TMPFILE $URL && tar -zxvf $TMPFILE xcaddy && chmod +x xcaddy
+go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 
 # xcaddy build
-./xcaddy build \
+xcaddy build \
 	--with github.com/mholt/caddy-l4 \
-	--with github.com/caddy-dns/cloudflare \
 	--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
-	
-# clean
-rm -rf go xcaddy
