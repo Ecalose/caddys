f8b2bb5fbf4b98418ac02b7001a1cdcbda3e93dc
Mcl <mixool0204@gmail.com>
2020-12-06 18:05:28 +0800

    init.

--- generate.sh
+++ generate.sh
@@ -6,16 +6,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/go/bin
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
 
 # go install
-wget -O $TMPFILE https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
-tar -zxf $TMPFILE
+wget -qO $TMPFILE https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+tar -zxvf $TMPFILE
 
 # xcaddy install
 URL="$(wget -qO- https://api.github.com/repos/caddyserver/xcaddy/releases/latest | grep -E "browser_download_url.*linux_amd64.tar.gz" | cut -f4 -d\")"
-rm -rf /usr/bin/xcaddy
-wget -O $TMPFILE $URL && tar -zxf $TMPFILE -C /usr/bin && chmod +x /usr/bin/xcaddy
+wget -qO $TMPFILE $URL && tar -zxvf $TMPFILE && chmod +x xcaddy
 
 # xcaddy build
-xcaddy build \
+./xcaddy build \
     --with github.com/mholt/caddy-l4 \
 	--with github.com/caddy-dns/cloudflare \
 	--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
