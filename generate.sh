57b1d6bf39dbeab88e25836117ce17a68c3cdc44
-- <mixool0204@gmail.com>
2021-02-18 12:53:52 +0800

    update

--- generate.sh
+++ generate.sh
@@ -7,9 +7,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/go/bin
 go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 
 # xcaddy build
-xcaddy build \
+xcaddy build latest \
     --with github.com/mholt/caddy-l4 \
     --with github.com/caddy-dns/cloudflare \
-    --with github.com/klzgrad/forwardproxy@naive
+    --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
 
 tar -czvf caddy.tar.gz caddy
