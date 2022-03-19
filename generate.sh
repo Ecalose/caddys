10213c55dfd7a1410f78719b5f016a50d0f1a7b1
Ecalose <47114714+Ecalose@users.noreply.github.com>
2022-03-19 13:04:38 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,19 +4,20 @@
 # only for https://github.com/mixool/caddys
 ######
 
-go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
+# go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 
 # xcaddy build
 xcaddy build latest \
     --with github.com/mholt/caddy-l4 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-webdav \
-    --with github.com/caddyserver/forwardproxy@caddy2
+    --with github.com/caddyserver/forwardproxy@caddy2 \
     --output ./caddy-amd64
 export GOOS=linux GOARCH=arm64
 xcaddy build latest \
     --with github.com/mholt/caddy-l4 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-webdav \
-    --with github.com/caddyserver/forwardproxy@caddy2
+    --with github.com/caddyserver/forwardproxy@caddy2 \
     --output ./caddy-arm64
